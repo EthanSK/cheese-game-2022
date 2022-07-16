a5c865905de4eeb7c957fadb157356ea1cde30bd
Ethan Sarif-Kattan <ethaneus99@gmail.com>
2022-07-16 17:16:24 +0100

    ascoeu

--- Cheese Game 2022/Assets/Enemy/Enemy.cs
+++ Cheese Game 2022/Assets/Enemy/Enemy.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int CurDiceFace => _curDiceFace;
+
+    [SerializeField] private List<Sprite> _diceFaces;
+    [SerializeField] private SpriteRenderer _diceSpriteRenderer;
+
+    private int _curDiceFace = 1;
+
+
+    private void Start()
     {
-        
+        InitDice();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void InitDice()
     {
-        
+        _curDiceFace = Random.Range(1, 7);
+        SetDiceFace();
     }
+
+    private void SetDiceFace()
+    {
+        _curDiceFace = (int)Mathf.Floor((transform.localRotation.eulerAngles.z % 360f) / (360f / (float)_diceFaces.Count)) + 1;
+        _diceSpriteRenderer.sprite = _diceFaces[_curDiceFace - 1];
+    }
+
 }
